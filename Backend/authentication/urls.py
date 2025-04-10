91a98e9d5d6a2f3423186dc527e79019c730ebc1
vsreddyh <shouryanreddyh@gmail.com>
2025-04-10 23:35:55 +0530

    Added rename user

--- Backend/authentication/urls.py
+++ Backend/authentication/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import create_user, delete_user
+from .views import create_user, delete_user, rename_user
 
 urlpatterns = [
     path("create-user", create_user, name="create-user"),
     path("delete-user", delete_user, name="delete-user"),
+    path("rename-user", rename_user, name="rename-user"),
 ]
--- Backend/authentication/views.py
+++ Backend/authentication/views.py
@@ -4,43 +4,44 @@ import json
 from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
 
 
 @csrf_exempt
+@require_http_methods(["POST"])
 def create_user(request):
-    if request.method == "POST":
-        try:
-            data = json.loads(request.body)
-            username = data.get("username")
-            password = data.get("password")
+    try:
+        data = json.loads(request.body)
+        username = data.get("username")
+        password = data.get("password")
 
-            if not username:
-                return JsonResponse({"error": "Username is required"}, status=500)
-            if not password:
-                return JsonResponse({"error": "Password is required"}, status=500)
+        if not username:
+            return JsonResponse({"error": "Username is required"}, status=500)
+        if not password:
+            return JsonResponse({"error": "Password is required"}, status=500)
 
-            is_first_user = User.objects.count() == 0
+        is_first_user = User.objects.count() == 0
 
-            if User.objects.filter(username=username).exists():
-                return JsonResponse({"error": "Username already exists"}, status=403)
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({"error": "Username already exists"}, status=403)
 
-            user = User.objects.create_user(username=username, password=password)
+        user = User.objects.create_user(username=username, password=password)
 
-            if is_first_user:
-                user.is_staff = True
-                user.is_superUser = True
-                user.save()
+        if is_first_user:
+            user.is_staff = True
+            user.is_superUser = True
+            user.save()
             return JsonResponse(
                 {"success": "Created Succesfully", "admin": is_first_user},
                 status=201,
             )
 
-        except Exception as e:
-            return JsonResponse({"error": str(e)}, status=500)
-    return JsonResponse({"error": "only POST accepted"}, status=405)
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=500)
 
 
 @csrf_exempt
+@require_http_methods(["DELETE"])
 def delete_user(request):
     try:
         data = json.loads(request.body)
@@ -60,5 +61,35 @@ def delete_user(request):
         user.delete()
         return JsonResponse({"success": f"User '{username}' deleted"}, status=200)
 
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=500)
+
+
+@csrf_exempt
+@require_http_methods(["PUT"])
+def rename_user(request):
+    try:
+        data = json.loads(request.body)
+        username = data.get("username")
+        newusername = data.get("newusername")
+
+        if not username:
+            return JsonResponse({"error": "Username is required"}, status=500)
+
+        if not newusername:
+            return JsonResponse({"error": "Username is required"}, status=500)
+
+        user = User.objects.filter(username=username).first()
+
+        if not user:
+            return JsonResponse({"error": "Username is required"}, status=500)
+
+        if User.objects.filter(username=newusername):
+            return JsonResponse({"error": "Username is already taken"}, status=500)
+
+        user.username = newusername
+        user.save()
+
+        return JsonResponse({"success": "Renamed Successfully"}, status=200)
     except Exception as e:
         return JsonResponse({"error": str(e)}, status=500)
